f337613f4b3f43457388df1079d2e90cde2e60ca
Suraj Shedge <surajshedge9356@gmail.com>
2025-02-08 11:41:47 +0530

    Updated package files and added config files

---
+++ src/config/gemini.js
@@ -0,0 +1,34 @@
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = process.env.VITE_GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response.text();
+    console.log(result.response.text());
+  }
+  
+export default run
